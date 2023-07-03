62f80f6158352fe5d98f1cc1ffd869eff57e7886
Evgeniy Degtyar <farappl@gmail.com>
2023-07-03 17:06:51 +0500

    add getHerosStats function

--- js/__tests__/healthbar.js
+++ js/__tests__/healthbar.js
@@ -1,10 +1,27 @@
-import showHealthBar from '../health';
+import getHealthStatus, { getHerosStats } from '../health';
 
 test.each([
   [{ name: 'Маг', health: 90 }, 'healthy'],
   [{ name: 'Воин', health: 10 }, 'critical'],
   [{ name: 'Лучник', health: 40 }, 'wounded'],
-])('tesing healthbar function with %o', (heroParams, expected) => {
-  const result = showHealthBar(heroParams);
+])('testing healthbar stat function with %o', (heroParams, expected) => {
+  const result = getHealthStatus(heroParams);
   expect(result).toBe(expected);
+});
+
+test('sorted heros stats', () => {
+  const healthData = [
+    { name: 'Маг', health: 90 },
+    { name: 'Воин', health: 10 },
+    { name: 'Лучник', health: 40 },
+  ];
+
+  const sortedHealthData = [
+    { name: 'Маг', health: 90 },
+    { name: 'Лучник', health: 40 },
+    { name: 'Воин', health: 10 },
+  ];
+
+  const result = getHerosStats(healthData);
+  expect(result).toEqual(sortedHealthData);
 });
--- js/health.js
+++ js/health.js
@@ -1,5 +1,10 @@
-export default function showHealthBar(heroHealth) {
+export default function getHealthStatus(heroHealth) {
   if (heroHealth.health < 15) return 'critical';
   if (heroHealth.health >= 15 && heroHealth.health <= 50) return 'wounded';
   return 'healthy';
+}
+
+export function getHerosStats(herosHealthData) {
+  herosHealthData.sort((a, b) => b.health - a.health);
+  return herosHealthData;
 }
